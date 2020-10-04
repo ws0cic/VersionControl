73b9aab3de35d9bf665c7df2c3e77ed6ccb24efc
Norbert Böttkös <norbert.bottkos@gmail.com>
2020-10-04 21:43:02 +0200

    #7 - fix wrong value

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -79,7 +79,7 @@ namespace UserMaintenance {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Utónév.
+        ///   Looks up a localized string similar to Teljes név.
         /// </summary>
         internal static string FullName {
             get {
