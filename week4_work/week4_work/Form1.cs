5f5aadd7bfa1b188aa943d67697c5d2d6e80d05c
Norbert Böttkös <norbert.bottkos@gmail.com>
2020-10-11 22:06:16 +0200

    #11 - delivere the week04 task

--- week4_work/week4_work/Form1.cs
+++ week4_work/week4_work/Form1.cs
@@ -1,20 +1,166 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace week4_work
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+     "Kód",
+     "Eladó",
+     "Oldal",
+     "Kerület",
+     "Lift",
+     "Szobák száma",
+     "Alapterület (m2)",
+     "Ár (mFt)",
+     "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = (f.Price * 1000000) / f.FloorArea;
+                counter++;
+            }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            FormatTable(headers);
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = string.Empty;
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormatTable(string[] headers)
+        {
+            Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            if (Flats.Count == 0)
+                return;
+
+            // First row
+            Range firstRow = xlSheet.get_Range(GetCell(2, 1), GetCell(2, headers.Length));
+            firstRow.Font.Bold = true;
+            firstRow.Interior.Color = Color.LightYellow;
+
+            // Last Row
+            Range lastRow = xlSheet.get_Range(GetCell(Flats.Count + 1, 1), GetCell(Flats.Count + 1, headers.Length));
+            lastRow.Interior.Color = Color.LightGreen;
+            lastRow.NumberFormat = "###.00";
+
+
+            Range allFlats = xlSheet.get_Range(GetCell(2, 1), GetCell(Flats.Count + 1, headers.Length));
+            Borders border = allFlats.Borders;
+            border[XlBordersIndex.xlEdgeBottom].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeBottom].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeTop].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeTop].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeLeft].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeLeft].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeRight].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeRight].Weight = XlBorderWeight.xlThick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateExcel();
         }
     }
 }
